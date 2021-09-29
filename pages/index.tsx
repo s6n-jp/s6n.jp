5ba0394b573ec798e83e0aabfc0998cc94920328
Naoki Ikeguchi <root@siketyan.dev>
2021-09-29 21:04:35 +0900

    ✨ Sass / SCSS Support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './index.module.scss';
 
 const Index: React.FC = () => {
   return (
-    <p>Hello, world!</p>
+    <p className={styles.hello}>Hello, world!</p>
   )
 };
 
