61f5168fd958749f68fc9dcf4b47a62bc895c1a1
Naoki Ikeguchi <me@s6n.jp>
2022-01-10 17:34:27 +0900

    ✨ Add favicon and manifests

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import Layout from '../components/templates/main'
 
@@ -8,9 +9,25 @@ import '../styles/index.scss';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#fe3a7c" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <meta name="apple-mobile-web-app-title" content="s6n.jp" />
+        <meta name="application-name" content="s6n.jp" />
+        <meta name="msapplication-TileColor" content="#fe3a7c" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+        <title>s6n.jp : engineering behind our web.</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
